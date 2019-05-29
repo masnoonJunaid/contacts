a4c8a7da36b295584ad2cda490d85f2b73fdf5ed
Masnoon Junaid <masnoon20@gmail.com>
2019-05-29 15:02:44 +0530

    add contact font

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
 import ListContacts from './ListContacts'
 import './App.css';
+import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
+// import gallary from './gallary'
 
 
 class App extends Component {
   state = {
+    screen:'list', /*or create or list*/
     contacts : []
   }
+  // fetching data with componentDidMount method from back-end server
   componentDidMount() {
     ContactsAPI.getAll().then((contacts) => {
       this.setState({contacts})
@@ -20,8 +24,21 @@ class App extends Component {
   }
   render(){
     return (
-      <div>
-        <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts} />
+      <div classNam="app">
+        {this.state.screen === 'list' && (
+          <ListContacts
+          onDeleteContact={this.removeContact}
+          contacts={this.state.contacts}
+          onNavigate={() => {
+            this.setState({screen:'create'})
+          }}
+          />
+
+        )}
+        {this.state.screen === 'create' && (
+          <CreateContact/>
+          // <gallary/>
+        )}
       </div>
     )
   }
---
+++ src/gallary.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class gallary extends Component {
+  render(){
+    return(
+      <img alt="" className="target" src="public/tobedone.jpg"></img>
+    )
+  }
+}
+
+export default gallary
