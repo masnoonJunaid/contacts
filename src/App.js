163768cce903585340bcf3724e31b976bb68e5cd
Masnoon Junaid <masnoon20@gmail.com>
2019-05-30 02:52:09 +0530

    add your contact to the list

--- src/App.js
+++ src/App.js
@@ -21,7 +21,17 @@ class App extends Component {
     this.setState((state)=>({
       contacts:state.contacts.filter((c) =>c.id!==contact.id)
     }))
+    ContactsAPI.remove(contact)
   }
+
+  CreateContact(contact) {
+    ContactsAPI.create(contact).then(contact =>{
+      this.setState(state => ({
+        contacts:state.contacts.concat({ contact })
+      }))
+    })
+  }
+
   render(){
     return (
       <div className="app">
@@ -32,7 +42,15 @@ class App extends Component {
           />
         )}/>
 
-      <Route path="/create" component={CreateContact}/>
+      <Route path="/create" render={( {history }) =>(
+          <CreateContact
+            onCreateContact={(contact) =>{
+                this.CreateContact(contact)
+                history.push('/')
+
+            }}
+            />
+        )}/>
       </div>
     )
   }
